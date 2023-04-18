428682d736521f23861239606d30d23997d4504e
vanshi03 <56712218+vanshi03@users.noreply.github.com>
2023-04-18 14:08:18 +0530

    Steganography UI changes

--- app/src/main/java/com/ayush/steganography/Decode.java
+++ app/src/main/java/com/ayush/steganography/Decode.java
@@ -37,6 +37,7 @@ public class Decode extends AppCompatActivity implements TextDecodingCallback {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_decode);
 
         //Instantiation of UI components
--- app/src/main/java/com/ayush/steganography/Encode.java
+++ app/src/main/java/com/ayush/steganography/Encode.java
@@ -54,6 +54,7 @@ public class Encode extends AppCompatActivity implements TextEncodingCallback {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_encode);
 
         //initialized the UI components
